56775d99252085a39eefda8f30e5952073f918c7
GaParmar <gparmar@ucsd.edu>
2018-02-03 00:32:15 -0800

    Completed the implementation of the song class

--- MediaFlashback/app/src/main/java/Song.java
+++ MediaFlashback/app/src/main/java/Song.java
@@ -3,8 +3,6 @@
  */
 
 // -1, NA, represents uninitialized value
-
-
 public class Song {
 
     enum state {
@@ -18,36 +16,111 @@ public class Song {
     // cover art
 
     private String title;
-    private String parent_album;
-    private state current_state;
+    private String parentAlbum;
+    private state currentState;
     private String location;    // TODO:: temporary way of storing location
-    private String artist_name;
-    private int length_in_seconds;
-    private int year_of_release;
-    private int time_last_played; // TODO:: temporary way of storing the time stamp
+    private String artistName;
+    private int lengthInSeconds;
+    private int yearOfRelease;
+    private int timeLastPlayed; // TODO:: temporary way of storing the time stamp
     private double probability; // TODO:: not yet implemented?
 
 
     /* the default constructor */
     public Song(){
         title = "";
-        parent_album = "NA";
-        current_state = state.NEITHER;
+        parentAlbum = "NA";
+        currentState = state.NEITHER;
         location = "NA";
-        artist_name = "";
-        length_in_seconds = -1;
-        year_of_release = -1;
-        time_last_played = -1;
+        artistName = "";
+        lengthInSeconds = -1;
+        yearOfRelease = -1;
+        timeLastPlayed = -1;
     }
 
-    public Song(String title, String parent_album,
-                String artist_name, int length_in_seconds,
-                int year_of_release){
+    public Song(String title, String parentAlbum,
+                String artistName, int lengthInSeconds,
+                int yearOfRelease){
         this();
         this.title = title;
-        this.parent_album = parent_album;
-        this.length_in_seconds = length_in_seconds;
-        this.artist_name = artist_name;
-        this.year_of_release = year_of_release;
+        this.parentAlbum = parentAlbum;
+        this.lengthInSeconds = lengthInSeconds;
+        this.artistName = artistName;
+        this.yearOfRelease = yearOfRelease;
     }
+
+    public String getTitle(){
+        return this.title;
+    }
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public String getParentAlbum(){
+        return this.parentAlbum;
+    }
+
+    public void setParentAlbul(String parentAlbum){
+        this.parentAlbum = parentAlbum;
+    }
+
+    public state getCurrentState(){
+        return this.currentState;
+    }
+
+    public void setCurrentState(state currentState){
+        this.currentState = currentState;
+    }
+
+    public String getLocation(){
+        return this.location;
+    }
+
+    public void setLocation(String location){
+        this.location = location;
+    }
+
+    public String getArtistName(){
+        return this.artistName;
+    }
+
+    public void setArtistName(String artistName){
+        this.artistName = artistName;
+    }
+
+    public int getLengthInSeconds(){
+        return this.lengthInSeconds;
+    }
+
+    public void setLengthInSeconds(int lengthInSeconds){
+        this.lengthInSeconds = lengthInSeconds;
+    }
+
+    public int getYearOfRelease(){
+        return this.yearOfRelease;
+    }
+
+    public void setYearOfRelease(int yearOfRelease){
+        this.yearOfRelease = yearOfRelease;
+    }
+
+    public int getTimeLastPlayed(){
+        return this.timeLastPlayed;
+    }
+
+    public void setTimeLastPlayed(int timeLastPlayed){
+        this.timeLastPlayed = timeLastPlayed;
+    }
+
+    public void updateProbability(){
+        double prob = 0.0;
+        // TODO:: the logic to calculate probability would go here
+        this.probability = prob;
+    }
+
+    public double getProbability(){
+        return this.probability;
+    }
+
 }
