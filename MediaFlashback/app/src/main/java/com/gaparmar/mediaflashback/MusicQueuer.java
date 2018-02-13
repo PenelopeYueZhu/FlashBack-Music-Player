3fd4fc26fc88e03a3190eb26d7da53c75c2d42f3
Veronica Lin <velin@ucsd.edu>
2018-02-12 16:35:58 -0800

    new class made

---
+++ MediaFlashback/app/src/main/java/com/gaparmar/mediaflashback/MusicQueuer.java
@@ -0,0 +1,25 @@
+package com.gaparmar.mediaflashback;
+
+import java.util.HashMap;
+
+/**
+ * Created by veronica.lin1218 on 2/12/2018.
+ */
+
+public class MusicQueuer {
+    private HashMap<Song, Integer> allTracks = new HashMap();
+    private HashMap<Album, Integer> allAlbums = new HashMap();
+    public MusicQueuer() {
+
+    }
+    public void readSongs() {
+
+    }
+
+    public Album getAlbum(String albumName) {
+        return Album;
+    }
+
+
+
+}
