564fe114b468c2d63a2e1dc0911db8e153378e12
GaParmar <gparmar@ucsd.edu>
2018-03-07 16:59:40 -0800

    replicated the original functionality in the new screen

--- MediaFlashback/app/src/main/java/com/gaparmar/mediaflashback/UINormal.java
+++ MediaFlashback/app/src/main/java/com/gaparmar/mediaflashback/UINormal.java
@@ -223,31 +223,6 @@ public class UINormal extends UIHandler {
             }
         });
 
-        /*ownloadBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Log.d("UINomarl", "downloadButton clicked");
-
-                Log.d("UINomarl",inputURL.getText().toString());
-                // need to have input
-                if (inputURL.getText() != null) {
-                    String url = inputURL.getText().toString();
-                    Toast downloadingToast = Toast.makeText(context, "Downloading from "
-                            + url, Toast.LENGTH_SHORT);
-                    downloadingToast.show();
-
-                    if (musicDownloader == null) {
-                        musicDownloader = MainActivity.getMusicDownloader();
-                    }
-                    musicDownloader.downloadData(url, "Song name", "mp3");
-
-                } else {
-                  // no url link provided
-                    Toast noURLToast = Toast.makeText(context, "Please enter URL", Toast.LENGTH_SHORT);
-                    noURLToast.show();
-                }
-            }
-        });*/
 
     }
 
