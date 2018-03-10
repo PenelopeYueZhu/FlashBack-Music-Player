60c969ce02ad964283d9a10fa64553016733b084
Veronica Lin <velin@ucsd.edu>
2018-03-09 21:46:53 -0800

    Added functionality between tabs

--- MediaFlashback/app/src/main/java/com/gaparmar/mediaflashback/FavoritesFragment.java
+++ MediaFlashback/app/src/main/java/com/gaparmar/mediaflashback/FavoritesFragment.java
@@ -1,12 +1,21 @@
 package com.gaparmar.mediaflashback;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.app.Fragment;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 
 /**
