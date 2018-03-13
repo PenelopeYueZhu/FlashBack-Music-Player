8a3b5f0f2f6b5537bc2b6a49782ccc497e3da94a
GaParmar <gparmar@ucsd.edu>
2018-03-12 23:17:20 -0700

    made the vibemode activity and added the intent to start the activity in the mainActivity

---
+++ MediaFlashback/app/src/main/java/com/gaparmar/mediaflashback/UI/VibemodeActivity.java
@@ -0,0 +1,15 @@
+package com.gaparmar.mediaflashback.UI;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.gaparmar.mediaflashback.R;
+
+public class VibemodeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vibemode);
+    }
+}
